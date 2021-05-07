3f752fdc9c35195ef2f3b631fe9035e673534c90
james <ksjjl05@gmail.com>
2021-05-07 14:27:03 -0500

    adding some js code samples

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-const options= [
-{id: "role:parent", invoice_name: "Role: Parent", description: "", prices: Array(2), requires: Array(0)},
+let label= [
+"Role: Parent",
+"Role: Player",
+"Role: Coach",
+"Role: Academy",
+]
+let arr=[
 {id: "role:player", invoice_name: "Role: Player", description: "", prices: Array(2), requires: Array(0)},
-{id: "role:coach", invoice_name: "Role: Coach", description: "", prices: Array(2), requires: Array(0)},
-{id: "role:academy", invoice_name: "Role: Academy", description: "", prices: Array(2), requires: Array(0)}]
+ {id: "role:parent", invoice_name: "Role: Parent", description: "", prices: Array(2), requires: Array(0)},
+ {id: "role:coach", invoice_name: "Role: Coach", description: "", prices: Array(2), requires: Array(0)},
+ {id: "role:academy", invoice_name: "Role: Academy", description: "", prices: Array(2), requires: Array(0)}
+]
+ 
+const arr2= arr.filter(item => item === arr[0])
+// console.log(arr2);
+const arr3 = arr.filter(item => item === arr[2])
+const arr4 = arr2.concat(arr3)
+console.log(arr4);
+let label1 = label.shift()
+let label2 = label.pop()
+label[0] = "Player/Parent";
+label[1] = "Coach/Agency";
+console.log(label);
 
-options.map((role, index) => {
-    let label = role.invoice_name.replace("Role: ", "");
-    let _id = role.id;
-    let _price = role.prices;
-    let _name = role.invoice_name;
-    return 
-      
-        {label} 
-          value={label}
-         
-   
-    
-  })
